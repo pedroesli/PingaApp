ade4166463e2344c1e670c49f86a3ebeeb6c3574
Pedro Esli <pedroesli.vn@gmail.com>
2022-07-15 16:23:12 -0300

    Created the project in xcode and added test command for CircleCi

---
+++ PingaApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PingaApp
+//
+//  Created by Pedro Ésli Vieira do Nascimento on 15/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PingaApp/PingaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PingaAppApp.swift
+//  PingaApp
+//
+//  Created by Pedro Ésli Vieira do Nascimento on 15/07/22.
+//
+
+import SwiftUI
+
+@main
+struct PingaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
