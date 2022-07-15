3a9da7a33d782d372fb2871e94924c3cda77d6fa
Nyffi <nyffiman@gmail.com>
2022-07-15 16:51:27 -0300

    feat: Fastlane implemented

---
+++ PingaApp/SecondContentView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondContentView.swift
+//  PingaApp
+//
+//  Created by Paulo César on 15/07/22.
+//
+
+import SwiftUI
+
+struct SecondContentView: View {
+    var body: some View {
+        Text("Segunda tela em SwiftUI")
+    }
+}
+
+struct SecondContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondContentView()
+    }
+}
--- PingaAppUITests/PingaAppUITests.swift
+++ PingaAppUITests/PingaAppUITests.swift
@@ -9,23 +9,40 @@ import XCTest
 
 class PingaAppUITests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+//    override func setUpWithError() throws {
+//        // Put setup code here. This method is called before the invocation of each test method in the class.
+//
+//        // In UI tests it is usually best to stop immediately when a failure occurs.
+//        continueAfterFailure = false
+//
+//        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+//    }
+//
+//    override func tearDownWithError() throws {
+//        // Put teardown code here. This method is called after the invocation of each test method in the class.
+//    }
+    
+    override func setUp() {
         let app = XCUIApplication()
+        setupSnapshot(app)
         app.launch()
+    }
+
+    func testScreenshot() throws {
+        // UI tests must launch the application that they test.
+        // snapshot("0Launch")
+        
+        let app = XCUIApplication()
+        let backButton = app.navigationBars["_TtGC7SwiftUI19UIHosting"].buttons["Back"]
+        
+        app.buttons["Segunda tela"].tap()
+        
+        snapshot("0Launch")
+        
+        backButton.tap()
+        
+        snapshot("1Launch")
+                
 
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
