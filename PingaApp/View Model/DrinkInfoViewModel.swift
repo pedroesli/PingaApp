d3cd7630db38cd9b7fc0d875d120b92202722963
Pedro Esli <pedroesli.vn@gmail.com>
2022-07-18 22:44:10 -0300

    Created DrinkInfoView

---
+++ PingaApp/View Model/DrinkInfoViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  DrinkInfoViewModel.swift
+//  PingaApp
+//
+//  Created by Pedro Ésli Vieira do Nascimento on 18/07/22.
+//
+
+import Foundation
+
+class DrinkInfoViewModel: ObservableObject {
+    
+    func initView(idDrink: String?) {
+        
+    }
+}
---
+++ PingaApp/View/DrinkInfoView.swift
@@ -0,0 +1,23 @@
+//
+//  DrinkInfoView.swift
+//  PingaApp
+//
+//  Created by Pedro Ésli Vieira do Nascimento on 18/07/22.
+//
+
+import SwiftUI
+
+struct DrinkInfoView: View {
+    
+    var idDrink: String?
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DrinkInfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        DrinkInfoView()
+    }
+}
